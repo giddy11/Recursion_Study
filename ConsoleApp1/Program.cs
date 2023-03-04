5fbe763e346ad09b748ff89cf0a0e95de25164fd
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-03-04 20:49:18 +0100

    recursion base on factorial

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int Factorial(int num)
+{
+    if (num == 1)
+        return 1;
+
+    return num * Factorial(num - 1);
+}
+
+Console.WriteLine(Factorial(5));
