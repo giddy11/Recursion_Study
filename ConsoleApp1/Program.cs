739e53eb72d3425b8f507f24bf8ec989e09fe5c1
Gideon Edoghotu <edoghotugiddy@gmail.com>
2023-03-05 14:12:03 +0100

    SumNmbers and number of steps

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+//Console.WriteLine(Factorial(5));
+//Console.WriteLine(Power(2, 4));
+//Console.WriteLine(Walk(5));
+Console.WriteLine(SumNumbers(5));
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    if (num == 0) return 1;
+
+    return num + SumNumbers(num - 1);
+}
+
+int Walk(int steps)
+{
+    if (steps < 1) return 1;
+    Console.WriteLine("You take a step!");
+    return Walk(steps - 1);
+}
+
 int Factorial(int num)
 {
     if (num == 1)
@@ -8,4 +29,11 @@ int Factorial(int num)
     return num * Factorial(num - 1);
 }
 
-Console.WriteLine(Factorial(5));
+float Power(float num, int powerOf)
+{
+    if (powerOf == 1)
+        return 1;
+
+    return num * Power(num, powerOf - 1);
+}
+
